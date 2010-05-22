d37a9d5e521c4220206fb4c31325ca9a4ce302e9
Knut Erik Borgen <knuterikborgen@Ranveig-Flatens-MacBook-Pro.local>
2010-05-22 09:54:54 +0200

    Property files for i18n texts.

---
+++ src/net/binarymuse/gwt/client/ui/wizard/WizardTexts.java
@@ -0,0 +1,44 @@
+package net.binarymuse.gwt.client.ui.wizard;
+
+/**
+ * Interface to represent the constants contained in resource bundle:
+ * 	'/Users/knuterikborgen/kode/gwt-wizard/src/net/binarymuse/gwt/client/ui/wizard/WizardTexts.properties'.
+ */
+public interface WizardTexts extends com.google.gwt.i18n.client.Constants {
+  
+  /**
+   * Translated "Cancel".
+   * 
+   * @return translated "Cancel"
+   */
+  @DefaultStringValue("Cancel")
+  @Key("cancel")
+  String cancel();
+
+  /**
+   * Translated "Finish".
+   * 
+   * @return translated "Finish"
+   */
+  @DefaultStringValue("Finish")
+  @Key("finish")
+  String finish();
+
+  /**
+   * Translated "Next >".
+   * 
+   * @return translated "Next >"
+   */
+  @DefaultStringValue("Next >")
+  @Key("next")
+  String next();
+
+  /**
+   * Translated "< Prev".
+   * 
+   * @return translated "< Prev"
+   */
+  @DefaultStringValue("< Prev")
+  @Key("previous")
+  String previous();
+}
